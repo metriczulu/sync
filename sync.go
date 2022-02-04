a9947e58b1374ef5a101a7d1d4398213b11d2aca
metriczulu <stephenson.shane.a@gmail.com>
2022-02-04 01:56:30 -0500

    added reverse to read config file func

--- sync.go
+++ sync.go
@@ -11,7 +11,8 @@ import (
 
 type Config map[string]map[string]string
 
-func ReadConfig(fileLocation string) (Config, error) {
+func ReadConfig(fileLocation string, reverse bool) (Config, error) {
+	// function to read a config file and parse it into a Config map
 	if fileLocation == "" {
 		return map[string]map[string]string {"": map[string]string {"": ""}}, nil
 	}
@@ -31,12 +32,16 @@ func ReadConfig(fileLocation string) (Config, error) {
 	for scanner.Scan() {
 		currLine = scanner.Text()
 		if (len(currLine) > 0) && (currLine[0] == '[') && (currLine[len(currLine)-1] == ']') {
-			currKey = currLine[1:len(currLine)-1]
+			currKey = strings.TrimSpace(currLine[1:len(currLine)-1])
 			configMap[currKey] = make(map[string]string)
 		} else if currLineVals = strings.Split(currLine, "="); len(currLineVals) > 1 {
-			firstVal = currLineVals[0]
-			secondVal = currLineVals[1]
-			configMap[currKey][firstVal] = secondVal
+			firstVal = strings.TrimSpace(currLineVals[0])
+			secondVal = strings.TrimSpace(currLineVals[1])
+			if reverse {
+				configMap[currKey][secondVal] = firstVal
+			} else {
+				configMap[currKey][firstVal] = secondVal
+			}
 		}
 	}
 	return configMap, nil
@@ -47,7 +52,7 @@ func main() {
 	unsyncFlag := flag.Bool("unsync", true, "Unsync the files.")
 	flag.Parse()
 
-	configMap, readErr := ReadConfig(*configFileLocation)
+	configMap, readErr := ReadConfig(*configFileLocation, true)
 	if readErr != nil {
 		fmt.Println("Error parsing config file:", readErr)
 	}
